07d9c6baeaf557ffd00be9962a845cdf1210f970
carolinpetersen <carolin.petersen@stud.leuphana.de>
2026-06-26 12:34:43 +0200

    matplot

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 import osmnx as ox
+import networkx as nx
 
-# Bereich definieren (z.B. Uni-Campus)
+# 1. Graph laden
 place = "Universität Leuphana, Lüneburg, Germany"
-graph = ox.graph_from_place(place, network_type='walk')
+G = ox.graph_from_place(place, network_type='walk')
 
-# Als GeoDataFrame exportieren (optional)
-nodes, edges = ox.graph_to_gdfs(graph)
+# 2. Start- und Zielpunkt (Koordinaten)
+start = (53.566, 9.984)  # Beispielkoordinaten
+end = (53.568, 9.987)
 
-# Visualisierung
-ox.plot_graph(graph)
+start_node = ox.nearest_nodes(G, start[1], start[0])
+end_node = ox.nearest_nodes(G, end[1], end[0])
+
+# 3. Kürzeste Route berechnen (z.B. nach Länge)
+route = nx.shortest_path(G, start_node, end_node, weight='length')
+
+# Route visualisieren
+ox.plot_graph_route(G, route)
+
+import matplotlib.pyplot as plt
+plt.show()
